43d927d160c78d7b10d95c66c8168c5d8962c670
Nithyasri2006 <indurunithyasri2006@gmail.com>
2024-07-31 19:45:07 +0530

    initial commit

---
+++ erp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import User from './components/User';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     < User name="nithya" type="student" branch="CSE" />
+         </div>
+  );
+}
+
+export default App;
---
+++ erp/src/components/User.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const User = (props) => {
+  return (
+    <div>
+      <h1> "welcome {props.name} logged in as {props.type} in {props.branch}" </h1>
+    </div>
+  )
+}
+
+export default User
+
+
+
+
+
